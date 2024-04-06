5c14acc4b48f2554e39baa46b04da3e659c82980
Nikita <n9507246@gmail.com>
2024-04-06 11:53:41 +0300

    компонент список новостей добавлен на тестовую страницу

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('Главная');
+$APPLICATION->SetTitle("Главная");
 $APPLICATION->SetPageProperty('title', 'Digital агентство We coders');
-?>
-    test
-
-<?
+?>test <br>
+ <?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"",
+Array()
+);?><br><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
