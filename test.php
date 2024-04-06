9570729bd353f774115cafddd66ce0831ce36c2f
Nikita <n9507246@gmail.com>
2024-04-06 12:36:28 +0300

    слайдер для главной страницы перемещён с тестовой станицы на главную

--- test.php
+++ test.php
@@ -3,10 +3,6 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
 $APPLICATION->SetPageProperty('title', 'Digital агентство We coders');
 ?>test <br>
- <?$APPLICATION->IncludeComponent(
-	"bitrix:news.list",
-	"",
-Array()
-);?><br><?
+ <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
