c02a09127d0ace46a9ee75906d47ebbac84fe8f3
Nikita Balashov <noWorkingEmail@mail.ru>
2024-04-08 11:19:51 +0500

    добавлены включаемые области для главной страницы и страницы О нас

--- about/index.php
+++ about/index.php
@@ -1,13 +1,40 @@
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('О нас');
-$APPLICATION->SetPageProperty('TITLE', 'О нас | We Coders');
-$APPLICATION->AddChainItem($APPLICATION->GetTitle(), $APPLICATION->GetCurDir());
+    require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+    $APPLICATION->SetTitle('О нас');
+    $APPLICATION->SetPageProperty('TITLE', 'О нас | We Coders');
+    $APPLICATION->AddChainItem($APPLICATION->GetTitle(), $APPLICATION->GetCurDir());
 
-$APPLICATION->SetPageProperty("keywords", "о нас, о компании, we coders, сайты");
+    $APPLICATION->SetPageProperty("keywords", "о нас, о компании, we coders, сайты");
 ?>
+    <section class="who-area-are pad-90" id="about_us">
+        <div class="container">
+            <h2 class="title-1">
+                <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                    "AREA_FILE_SHOW"   => "page",
+                    "AREA_FILE_SUFFIX" => "about_title"
+                ]); ?>
+            </h2>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="who-we">
+                        <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                            "AREA_FILE_SHOW"   => "page",
+                            "AREA_FILE_SUFFIX" => "about_text"
+                        ]); ?>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="about-bg">
+                        <? $APPLICATION->IncludeComponent("bitrix:main.include", "", [
+                            "AREA_FILE_SHOW"   => "page",
+                            "AREA_FILE_SUFFIX" => "about_image"
+                        ]); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    О нас
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+    require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
