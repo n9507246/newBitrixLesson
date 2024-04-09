9def9404d0c53c23f76f36af3c6c2331b6312334
Nikita Balashov <noWorkingEmail@mail.ru>
2024-04-09 13:37:05 +0500

    создана страница портфолио

---
+++ portfolio/index.php
@@ -0,0 +1,9 @@
+<?
+    require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+    $APPLICATION->SetTitle('Главная');
+    $APPLICATION->SetPageProperty('title', 'Портфолио | We coders');
+?>
+    Портфолио 
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
