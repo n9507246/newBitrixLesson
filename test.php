e0355229b85e73d61fbf73810a344eb11a992fc3
Nikita Balashov <noWorkingEmail@mail.ru>
2024-04-11 10:42:05 +0500

    создан компонент обратной связи

--- test.php
+++ test.php
@@ -2,7 +2,29 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Главная");
 $APPLICATION->SetPageProperty('title', 'Digital агентство We coders');
-?>test <br>
- <?
+?><br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ test<br>
+ <?$APPLICATION->IncludeComponent(
+	"test:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "n9507246@gmail.com",
+		"EVENT_MESSAGE_ID" => array(),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(),
+		"USE_CAPTCHA" => "N"
+	)
+);?><br><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
