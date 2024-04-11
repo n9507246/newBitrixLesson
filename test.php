148704c1a989190ef0b1de7cddca4fce157e56cc
Nikita Balashov <noWorkingEmail@mail.ru>
2024-04-11 11:16:12 +0500

    создана страница Контакты и добавлена форма обратной связи

--- test.php
+++ test.php
@@ -15,16 +15,6 @@ $APPLICATION->SetPageProperty('title', 'Digital агентство We coders');
  <br>
  <br>
  test<br>
- <?$APPLICATION->IncludeComponent(
-	"test:main.feedback",
-	"",
-	Array(
-		"EMAIL_TO" => "n9507246@gmail.com",
-		"EVENT_MESSAGE_ID" => array(),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array(),
-		"USE_CAPTCHA" => "N"
-	)
-);?><br><?
+<br><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
